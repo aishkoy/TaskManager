2f52f76d6db2663dc2a96019f314532392bf3166
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-03-08 17:34:39 +0600

    refactor: created additional constructor

--- src/models/Task.java
+++ src/models/Task.java
@@ -3,25 +3,31 @@ package models;
 import models.enums.TaskType;
 
 public class Task {
-    private final int id;
+    private int id;
     private int dayNum;
     private String title;
     private TaskType taskType;
 
     private String description;
 
-    public Task(int id,int dayNum, String title, TaskType taskType, String description) {
-        this.id = id;
+    public Task(int dayNum, String title, TaskType taskType, String description) {
         this.dayNum = dayNum;
         this.title = title;
         this.description = description;
         this.taskType = taskType;
+    }
+    public Task(int id,int dayNum, String title, TaskType taskType, String description) {
+        this(dayNum, title, taskType, description);
+        this.id = id;
     }
 
     public void setTaskType(TaskType taskType) {
         this.taskType = taskType;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
     public int getId() {
         return id;
     }
